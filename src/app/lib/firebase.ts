c8a0a73c5da560695297e0a91f8c7da875860c29
sidpadala <spotifysid10@gmail.com>
2025-04-06 06:11:10 -0500

    firebase config

---
+++ src/app/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAm8Y9xGJ9XsQviaqcttv0CT6BotpzgnFI",
+  authDomain: "promptbricks.firebaseapp.com",
+  projectId: "promptbricks",
+  storageBucket: "promptbricks.firebasestorage.app",
+  messagingSenderId: "1073036979446",
+  appId: "1:1073036979446:web:d7c7510f6a6e50bdccd0d7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
